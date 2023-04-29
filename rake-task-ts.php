44c270f1804a6b86b63a7a81fb4a945ce8dc2adc
ForBee.dev <code@forbee.dev>
2023-04-29 15:04:35 +0000

    new file:   Initial commit

---
+++ rake-task-ts.php
@@ -0,0 +1,10 @@
+<?php
+
+/*  Plugin Name: Rake Task TS
+    Plugin URI:
+    Description: Rake Task TS
+    Version: 1.0
+    Author: Tiago Santos
+    Author URI: https://github.com/forbee-dev
+    License: GPLv2 or later
+*/     
